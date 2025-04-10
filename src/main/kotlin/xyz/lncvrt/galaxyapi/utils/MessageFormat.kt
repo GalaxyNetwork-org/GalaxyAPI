dfd8b1664b63e4f25df39c500752a2bbfa7a1100
Lncvrt <lncvrtreal@gmail.com>
2025-04-09 22:25:46 -0700

    Fix name format (colors were wrong)

--- src/main/kotlin/xyz/lncvrt/galaxyapi/utils/MessageFormat.kt
+++ src/main/kotlin/xyz/lncvrt/galaxyapi/utils/MessageFormat.kt
@@ -4,7 +4,7 @@ package xyz.lncvrt.galaxyapi.utils
 class MessageFormat {
     companion object {
         fun name(bold: Boolean): String {
-            val name = "<gradient:#${Colors.PRIMARY}:#${Colors.SECONDARY}:#${Colors.PRIMARY}>GalaxyNetwork</gradient"
+            val name = "<gradient:#${Colors.SECONDARY}:#${Colors.PRIMARY}:#${Colors.SECONDARY}>GalaxyNetwork</gradient"
             return if (bold) {
                 "<bold>${name}</bold>"
             } else {
@@ -21,7 +21,7 @@ class MessageFormat {
         }
 
         fun setStaffPrefix(text: String): String {
-            return "<bold><gradient:#${Colors.PRIMARY}:#${Colors.SECONDARY}:#${Colors.PRIMARY}>GalaxyNetwork</gradient> <gradient:green:dark_green:green>Staff Mode</gradient> <gray>»</gray></bold> $text"
+            return "<bold>${name(false)} <gradient:green:dark_green:green>Staff Mode</gradient> <gray>»</gray></bold> $text"
         }
 
         fun setPrimaryColor(text: String): String {
